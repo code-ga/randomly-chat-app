f2872f35678cff9634d39acb6ae032b5f7658395
Code-ga <tritranduc11@gmail.com>
2025-01-14 00:30:06 +0700

    add docker and i don't tested now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://backend:3000",
+        changeOrigin: true,
+      },
+    }
+  }
 });
