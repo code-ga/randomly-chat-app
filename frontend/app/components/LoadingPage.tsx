58b33984aefcbb108535c16e16a0446ccadb7f07
Code-ga <tritranduc11@gmail.com>
2025-01-15 00:05:24 +0700

    change ws url

--- frontend/app/components/LoadingPage.tsx
+++ frontend/app/components/LoadingPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
       <div className="w-24 h-24 border-4 border-t-blue-500 border-b-transparent rounded-full animate-spin"></div>
       <p className="mt-8 text-lg text-gray-500">Loading...</p>
     </div>
--- frontend/app/context/ws.tsx
+++ frontend/app/context/ws.tsx
@@ -19,7 +19,9 @@ export const wsContext = React.createContext<wsContext>({
 
 export const useWs = () => React.useContext(wsContext);
 
-const URL = "ws://localhost:3000/ws";
+const URL = import.meta.env.DEV
+  ? "wss://randomize-chat-app.nbth.hackclub.app/ws"
+  : "ws://localhost:3000/ws";
 declare global {
   interface Window {
     client?: WebSocket | null;
