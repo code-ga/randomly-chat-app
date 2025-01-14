afbd953be8b3ebc5f675e20ab518a65170b33e2b
Code-ga <tritranduc11@gmail.com>
2025-01-14 23:54:49 +0700

    update for vercel v2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,22 @@ import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import vercel from 'vite-plugin-vercel';
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild, command }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+        input: "./server/app.ts",
+      }
+      : undefined,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), vercel()],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
   },
-});
+  plugins: [reactRouter(), tsconfigPaths()],
+}));
